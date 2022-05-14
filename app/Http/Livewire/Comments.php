3e48c8e80eb25709217155c2c427b4656ca3770c
Arvic S. Babol <kulotsystems@gmail.com>
2022-05-14 20:18:48 +0800

    Introduced model and event in comments section.

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
@@ -13,6 +14,19 @@ class Comments extends Component
             'creator'    => 'kulotsystems'
         ]
     ];
+    public $newComment;
+
+
+    public function addComment()
+    {
+        array_unshift($this->comments, [
+            'body'       => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator'    => 'kulotsystems'
+        ]);
+
+        $this->newComment = '';
+    }
 
 
     public function render()
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -2,9 +2,19 @@
     <div class="w-6/12">
         <h1 class="my-10 text-3xl">Comments</h1>
         <div class="my-4 flex">
-            <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What's in your mind?"/>
+            <input
+                wire:model="newComment"
+                type="text"
+                class="w-full rounded border shadow p-2 mr-2 my-2"
+                placeholder="What's in your mind?"
+            />
             <div class="py-2">
-                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
+                <button
+                    wire:click="addComment"
+                    class="p-2 bg-blue-500 w-20 rounded shadow text-white"
+                >
+                    Add
+                </button>
             </div>
         </div>
         @foreach($comments as $comment)
