13f9d982c8a4779b5b0124c1adeb9ff4e7fb2eae
Arvic S. Babol <kulotsystems@gmail.com>
2022-05-15 15:48:06 +0800

    Implemented mount event, fetched data from database.

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,30 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
-    public $comments = [
-        [
-            'body'       => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cum eligendi hic illum incidunt laboriosam magni, minus nemo neque nobis, obcaecati quaerat, repellat suscipit tempore temporibus tenetur ullam veniam voluptas.',
-            'created_at' => '3 min ago',
-            'creator'    => 'kulotsystems'
-        ]
-    ];
+    public $comments = [];
     public $newComment;
+
+
+    public function mount()
+    {
+        $this->comments = Comment::all();
+    }
 
 
     public function addComment()
     {
         if($this->newComment == '') return;
 
-        array_unshift($this->comments, [
+        /* array_unshift($this->comments, [
             'body'       => $this->newComment,
             'created_at' => Carbon::now()->diffForHumans(),
             'creator'    => 'kulotsystems'
-        ]);
+        ]); */
 
         $this->newComment = '';
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="{{ asset('tailwindcss/3.0.24.js') }}"></script>
-    @livewireStyles
+    <livewire:styles/>
     <title>Livewire</title>
 </head>
 <body>
-    @livewire('comments')
-    @livewireScripts
+    <livewire:comments/>
+    <livewire:scripts/>
 </body>
 </html>
