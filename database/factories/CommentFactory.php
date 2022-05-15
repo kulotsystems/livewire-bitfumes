b596a855376c12690c7043fda1b758fd19747ad3
Arvic S. Babol <kulotsystems@gmail.com>
2022-05-15 15:26:06 +0800

    Created Comment model, migration, and seeder.

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'body'    => 'This is sample comment',
+            'user_id' => 1
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name'   => 'Kulot Systems',
+             'email'  => 'test@kulotsystems.tech',
+         ]);
+
+        Comment::factory()->create([
+            'body'    => 'This is first comment',
+            'user_id' => 1,
+        ]);
     }
 }
